b01e9d78a30c80510fd14c89e05b3b1b8c201e4f
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-10-14 18:48:29 +0100

    add ex1

--- Labsheet 4/Labsheet 4/Program.cs
+++ Labsheet 4/Labsheet 4/Program.cs
@@ -4,7 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
-}
+        // simple program using methods which
+        // 1) displays a line of asterisks and
+        // 2) displays information about your college
+
+        string collegeInfo;
+        DisplayAsterisk();
+        collegeInfo = GetCollegeInfo();
+        Console.WriteLine(collegeInfo);
+        DisplayAsterisk();
+        Console.ReadLine();
+    } //end of main method
+
+
+    public static void DisplayAsterisk()
+    {
+
+     Console.WriteLine("**************************");
+
+    }//end of method
+
+    public static string GetCollegeInfo()
+    {
+        string returnValue;
+        returnValue = "College name = ATU Sligo\n" +
+                      "School colours = Red/Black\n" +
+                      "Location = Ash Lane, Sligo\n" +
+                      "Number of students = 7,000";
+
+        return returnValue;
+    }//end of method
+
+
+
+
+
+} // end of class
+
 
