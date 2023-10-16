61890cf2328426842aad5e183fed938c6235af4f
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-10-16 11:51:45 +0100

    add ex2

---
+++ Labsheet 4/Ex2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Ex2;
+
+class NameAndSaying
+{
+    static void Main(string[] args)
+    {
+        //setup
+        string name, saying;
+
+        //output
+        Console.WriteLine("><><><><><><><><><><><><><><><><><><><");
+
+        name = GetInputValue("name");
+        saying = GetInputValue("favourite saying");
+
+        DisplayNameAndSaying(name,saying);
+
+        Console.WriteLine("><><><><><><><><><><><><><><><><><><><");
+        Console.ReadLine();
+    }
+
+    static string GetInputValue(string whichValue)
+    {
+        string inputValue;
+
+        Console.Write("Enter your {0}> ", whichValue);
+        inputValue = Console.ReadLine();
+
+        return inputValue;
+    }
+
+    static void DisplayNameAndSaying(string name, string saying)
+    {
+
+        Console.WriteLine($"{name}'s favourite saying is {saying}");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}//end of class
+
+
