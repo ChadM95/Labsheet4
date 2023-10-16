ca47ce0f8b7ec289b1c96e1831a865559b267c1d
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-10-16 19:22:58 +0100

    add ex5

---
+++ Labsheet 4/Ex3/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Ex3;
+
+class Program
+{
+    //main method, GetSize, ComputeArea, ComputePerimeter, display
+
+    static void Main(string[] args)
+    {
+        double width, length, area, perimeter;     ;
+
+        width = GetSize("width");
+        length = GetSize("length");
+
+
+
+        Display(length, width, area, perimeter);
+        Console.ReadLine();
+    }
+
+    static double GetSize(string userValue)
+    {
+        double input;
+        Console.Write("Please enter {0}", userValue);
+        input = double.Parse(Console.ReadLine());
+
+        return input;
+    }
+
+    static double ComputeArea(double, double)
+
+
+
+
+
+
+
+    static void Display(double, double, double, double)
+    {
+        Console.WriteLine($"length:{length}\nWidth: {width} \narea: {area} \nperimeter: {perimeter}");
+    }
+}
+
---
+++ Labsheet 4/Ex5/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Ex5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Temperature Converter App");
+
+        double fahrenheit, celsius;
+
+        fahrenheit = GetFahrenheit();
+
+        celsius = ConvertToCelsius(fahrenheit);
+
+        Display(fahrenheit, celsius);
+        
+        Console.ReadLine();
+    }
+
+
+    static double GetFahrenheit()
+    {
+        Console.Write("Enter temperature in fahrenheit> ");
+        return double.Parse(Console.ReadLine());
+    }
+
+
+    static double ConvertToCelsius(double fahrenheit)
+    {
+        return fahrenheit * 0.38;
+    }
+
+
+    static void Display(double fahrenheit, double celsius)
+    {
+        Console.WriteLine($"Fahrenheit: {fahrenheit} \nCelsius: {celsius}");
+
+    }
+
+
+
+}
+
