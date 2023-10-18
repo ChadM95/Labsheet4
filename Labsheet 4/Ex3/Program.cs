1c19d9071cda892c3e3fad8d79fd88ceb3d7ecc1
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-10-18 18:44:58 +0100

    finish ex3

--- Labsheet 4/Ex3/Program.cs
+++ Labsheet 4/Ex3/Program.cs
@@ -11,32 +11,34 @@ class Program
         width = GetSize("width");
         length = GetSize("length");
 
+        area = ComputeArea(length, width);
 
-
+        perimeter = ComputePerimeter(length, width);
+     
         Display(length, width, area, perimeter);
+
         Console.ReadLine();
     }
 
     static double GetSize(string userValue)
     {
-        double input;
         Console.Write("Please enter {0}", userValue);
-        input = double.Parse(Console.ReadLine());
-
-        return input;
+        return double.Parse(Console.ReadLine());
     }
 
-    static double ComputeArea(double, double)
+    static double ComputeArea(double width, double length)
+    {
+        return width * length;
+    }
 
+    static double ComputePerimeter(double length, double width)
+    {
+        return (length * 2) + (width * 2); 
+    }
 
-
-
-
-
-
-    static void Display(double, double, double, double)
+    static void Display(double length, double width, double area, double perimeter)
     {
         Console.WriteLine($"length:{length}\nWidth: {width} \narea: {area} \nperimeter: {perimeter}");
     }
-}
 
+}
