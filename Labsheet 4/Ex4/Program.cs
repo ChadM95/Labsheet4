d84c3b39a02eebae1a1e4cfe0785c4be868fba60
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-10-18 19:06:33 +0100

    add first couple of methods

--- Labsheet 4/Ex4/Program.cs
+++ Labsheet 4/Ex4/Program.cs
@@ -2,9 +2,45 @@
 
 class TimeConverter
 {
+    //Main, InputSeconds, DetermineHours, DetermineRemainingSeconds
+    //DetermineNumberMinutes, Display
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Time Converter");
+
+        int totalSeconds, hours ;
+
+        totalSeconds = InputSeconds();
+
+        hours = DetermineHours(totalSeconds);
+
+        Display(hours);
+
+        Console.ReadLine();
     }
+
+    static int InputSeconds()
+    {
+        Console.Write("Total Seconds: ");
+        return int.Parse(Console.ReadLine());
+
+    }
+
+    static int DetermineHours(int totalSeconds)
+    {
+        return totalSeconds / 60 / 60;
+    }
+
+
+    static void Display(int hours)
+    {
+        Console.WriteLine(hours);
+    }
+
+
 }
+
+
+
 
